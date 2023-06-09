c14437ad0e9eef89154b2431c17c04cfb2a13b69
Pavel Slepushkin <pavel.slepushkin@gmail.com>
2023-06-09 17:02:10 +0200

    add autocorrection to keychron c1

--- keyboards/keychron/c1/plain/keymaps/spv/keymap.c
+++ keyboards/keychron/c1/plain/keymaps/spv/keymap.c
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
+#include "features/autocorrection.h"
 
 #define KC_TASK LGUI(KC_TAB)        // Task viewer
 #define KC_FLXP LGUI(KC_E)          // Windows file explorer
@@ -49,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
 
     [WIN_BASE] = LAYOUT( \
-        KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_VOLD,  KC_VOLU, \
+        KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_MPLY,  KC_VOLD,  KC_VOLU, \
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP, \
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN, \
         CAPSWRD,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,                                \
@@ -111,3 +112,9 @@ void caps_word_set_user(bool active) {
   }
 }
 
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+  if (!process_autocorrection(keycode, record)) { return false; }
+  // Your macros...
+
+  return true;
+}
